611dedf009bea757b72cf5d5afbb79e47de793d2
Pubudu Sanka <pubudusanka79@gmail.com>
2025-06-08 00:04:25 +0530

    Update AdminController.java

--- src/main/java/com/example/UserManagementApp/controller/AdminController.java
+++ src/main/java/com/example/UserManagementApp/controller/AdminController.java
@@ -1,6 +1,13 @@
 package com.example.UserManagementApp.controller;
 
+import com.example.UserManagementApp.dto.RegisterRequestDto;
+import com.example.UserManagementApp.dto.UserDto;
+import com.example.UserManagementApp.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/admin")
 @PreAuthorize("hasRole('ADMIN')")
 public class AdminController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/registeradminuser")
+    public ResponseEntity<UserDto> registerAdminUser(@RequestBody RegisterRequestDto registerRequestDto){
+        return ResponseEntity.ok(authService.registerAdminUser(registerRequestDto));
+    }
 }
