becb8c2976ced8c75181207c5549347b2ff79d2a
Pubudu Sanka <pubudusanka79@gmail.com>
2025-06-08 00:04:10 +0530

    Create RegisterRequestDto.java

---
+++ src/main/java/com/example/UserManagementApp/dto/RegisterRequestDto.java
@@ -0,0 +1,24 @@
+package com.example.UserManagementApp.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class RegisterRequestDto {
+
+    @NotBlank
+    @Size(min = 2, max = 50)
+    private String username;
+
+    @NotBlank
+    @Size(max= 70)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 8, max = 50)
+    private String password;
+
+}
