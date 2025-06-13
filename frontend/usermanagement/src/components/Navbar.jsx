d08dc4b35fb63b83396434ab103070c7f43e605a
Pubudu Sanka <pubudusanka79@gmail.com>
2025-06-13 17:43:29 +0530

    Update Navbar.jsx

---
+++ frontend/usermanagement/src/components/Navbar.jsx
@@ -0,0 +1,44 @@
+import { Link, useNavigate } from 'react-router-dom';
+import {authService} from '../services/Authservices';
+
+const Navbar = () => {
+
+    const navigate = useNavigate();
+
+    const currentUser = authService.getCurrentUser();
+
+    const handleLogout = async () => {
+        try {
+            await authService.logout();
+            navigate('/login');
+        } catch (error) {
+            console.error("Logout failed", error);
+        }
+    };
+
+    return (
+        <nav className="navbar">
+            <Link to="/" className='navbar-brand'>User Management</Link>
+            <div className="navbar-links">
+                {currentUser ? (
+                    <>
+
+                    <span className='navbar-user'> Welcome, {currentUser.username}</span>
+                    <Link to ="/dashboard" className='navbar-link'>Dashboard</Link>
+                    <button onClick={handleLogout} className='logout-button'>Logout</button>
+
+                    </>
+                ) : (
+                    <>
+                    <Link to="/login" className='navbar-link'>Login</Link>
+                    <Link to="/signup" className='navbar-link'>Signup</Link>
+                    </>
+                )}
+
+            </div>
+
+        </nav>
+    );
+};
+
+export default Navbar;
