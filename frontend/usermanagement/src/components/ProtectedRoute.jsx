3f4d2bbe5cea552329b9cd049305c97fddfd0727
Pubudu Sanka <pubudusanka79@gmail.com>
2025-06-13 17:46:50 +0530

    Update ProtectedRoute.jsx

---
+++ frontend/usermanagement/src/components/ProtectedRoute.jsx
@@ -0,0 +1,12 @@
+import { Navigate } from 'react-router-dom';
+import {authService} from '../services/authService';
+
+const ProtectedRoute = ({ children }) => {
+    const isAuthenticated = authService.isAuthenticated();
+    if (!isAuthenticated) {
+        return <Navigate to ="/login"/>
+    }
+    return children;
+};
+
+export default ProtectedRoute;
