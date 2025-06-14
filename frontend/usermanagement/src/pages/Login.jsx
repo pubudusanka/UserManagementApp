8e683559ae058f9d71369959e8bee7de93179141
Pubudu Sanka <pubudusanka79@gmail.com>
2025-06-14 21:37:32 +0530

    Update Login.jsx

--- frontend/usermanagement/src/pages/Login.jsx
+++ frontend/usermanagement/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { Link, useNavigate } from "react-router-dom";
-import { authService } from '../services/Authservices'
+import { authService } from '../services/Authservices';
+import '../styles/Login.css';
 
 const Login = () => {
     const [username, setUsername] = useState('');
